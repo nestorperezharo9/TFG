75b1ad232acad540a93b9dbe7e434ce692606650
nestor <nestorperezharo9@gmail.com>
2022-03-13 22:03:27 +0000

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
-    BodyComponent
+    FooterComponent
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
